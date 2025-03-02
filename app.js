ffc5faa41a91111b7751d6a77eabbe938701e660
Aditya Khandekar <khandekar.aditya1475@gmail.com>
2025-03-02 16:21:46 +0530

    added a button removed default grid sq sizes js functions added

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 let container = document.querySelector("#container");
-for(let i=0; i<256; i++) {
-    let innerdiv = document.createElement("div");
-    innerdiv.classList.add("grid-square");
+let resizeButton = document.querySelector("#resizeButton");
 
-    innerdiv.addEventListener("mouseenter", () => {
-        innerdiv.style.backgroundColor = "gray";
-    });
-    // innerdiv.addEventListener("mouseleave", () => {
-    //     innerdiv.style.backgroundColor = "white";
-    // });
-    container.appendChild(innerdiv);
+function createGrid(size) {
+    container.innerHTML = "";
+    const squareSize = 400/size;
+    for(let i=0; i<size*size; i++) {
+        let innerdiv = document.createElement("div");
+        innerdiv.classList.add("grid-square");
+
+        innerdiv.style.width = `${squareSize}px`;
+        innerdiv.style.height = `${squareSize}px`;
+    
+        innerdiv.addEventListener("mouseenter", () => {
+            innerdiv.style.backgroundColor = "gray";
+        });
+        container.appendChild(innerdiv);
+    }
 }
+
+createGrid(16);
+
+resizeButton.addEventListener("click", () => {
+    let newSize = prompt("Enter the number of squares per side (1-100): ");
+    newSize = parseInt(newSize);
+    if(isNaN(newSize) || newSize < 1 || newSize > 100) {
+        alert("Please enter a valid number between 1 to 100");
+        return;
+    }
+    createGrid(newSize);
+})

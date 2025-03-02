463c0ef5a3f4b03a7e76766ab1b2601584c4ce08
Aditya Khandekar <khandekar.aditya1475@gmail.com>
2025-03-02 10:06:00 +0530

    created grid setup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let container = document.querySelector("#container");
-for(let i=0; i<255; i++) {
+for(let i=0; i<256; i++) {
     let innerdiv = document.createElement("div");
     innerdiv.classList.add("grid-square");
     container.appendChild(innerdiv)

2b0b7bb4c5c4aed5646bb4402b244d79747306e8
Aditya Khandekar <khandekar.aditya1475@gmail.com>
2025-03-02 16:00:27 +0530

    Added hovering effect to the grid boxes using JS

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ let container = document.querySelector("#container");
 for(let i=0; i<256; i++) {
     let innerdiv = document.createElement("div");
     innerdiv.classList.add("grid-square");
-    container.appendChild(innerdiv)
+
+    innerdiv.addEventListener("mouseenter", () => {
+        innerdiv.style.backgroundColor = "gray";
+    });
+    // innerdiv.addEventListener("mouseleave", () => {
+    //     innerdiv.style.backgroundColor = "white";
+    // });
+    container.appendChild(innerdiv);
 }
